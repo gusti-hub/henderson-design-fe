ba642705da9a89fe5502690d139c898a3b5c4585
gusti-hub <agustianggaraputra@gmail.com>
2024-12-13 01:41:41 +0700

    commit

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,5 +1,5 @@
 //export const backendServer = "http://localhost:5000";   
 
-export const backendServer = "http://164.92.85.236/:5000";
+export const backendServer = "http://164.92.85.236:5000";
 
 //export const backendServer = "http://67.205.152.39:5000";
